30cec886abcdebcf6350497fdb1f4c81bd9c1f8a
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2022-07-11 09:42:26 -0300

    Project created

---
+++ BackEnd/src/main/java/com/dev/DSRank/DsRankApplication.java
@@ -0,0 +1,13 @@
+package com.dev.DSRank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsRankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsRankApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/dev/DSRank/DsRankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.DSRank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsRankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
