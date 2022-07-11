d8a361f97dfab402685f779b6cd5487c5b304b84
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2022-07-11 12:52:04 -0300

    useState

--- FrontEnd/src/components/SalesCard/index.tsx
+++ FrontEnd/src/components/SalesCard/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import NotificationButton from '../NotificationButton'
@@ -5,6 +6,13 @@ import NotificationButton from '../NotificationButton'
 import './styles.css'
 
 function SalesCard() {
+
+    const min = new Date(new Date().setDate(new Date().getDate() - 730));
+    const max = new Date();
+
+    const [minDate, setMinDate] = useState(min);
+    const [maxDate, setMaxDate] = useState(max);
+
     return (
         <>
             <div className="dsrank-card">
@@ -12,16 +20,16 @@ function SalesCard() {
                 <div>
                     <div className="dsrank-form-control-container">
                         <DatePicker
-                            selected={new Date()}
-                            onChange={(date: Date) => { }}
+                            selected={minDate}
+                            onChange={(date: Date) => setMinDate(date)}
                             className="dsrank-form-control"
                             dateFormat="dd/MM/yyyy"
                         />
                     </div>
                     <div className="dsrank-form-control-container">
                         <DatePicker
-                            selected={new Date()}
-                            onChange={(date: Date) => { }}
+                            selected={maxDate}
+                            onChange={(date: Date) => setMaxDate(date)}
                             className="dsrank-form-control"
                             dateFormat="dd/MM/yyyy"
                         />
