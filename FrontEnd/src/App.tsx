87a28234614cab451ee298a367d12e70ba245693
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2022-07-11 10:44:49 -0300

    First component

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,8 +1,13 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
   return (
-    <h1> Project DSRank</h1>
-    
+    <>
+      <h1> Project DSRank</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
