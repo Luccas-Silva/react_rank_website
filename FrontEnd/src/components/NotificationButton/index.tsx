00b07f9faf68e144d4ff6a501d2b81be11a29260
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2022-07-16 02:20:07 -0300

    Send notification

--- FrontEnd/src/components/NotificationButton/index.tsx
+++ FrontEnd/src/components/NotificationButton/index.tsx
@@ -2,14 +2,25 @@ import icon from '../../assets/img/notification-icon.svg'
 
 import './styles.css'
 
-function NotificationButton() {
-    return (
-        <>
-          <div className="dsrank-red-btn">
-            <img src={icon} alt="Notificar" />
-          </div>
-        </>
-    )
+type Props = {
+  saleId: number;
+}
+
+function handleClick(id: number) {
+  axios('${BASE_URL}/sales/${id}/notification')
+    .then(() => {
+      toast.info("SMS enviado com sucesso");
+    });
+}
+
+function NotificationButton({saleId} : Props) {
+  return (
+    <>
+      <div className="dsrank-red-btn" onClick={() => hendleClick(saleId)}>
+        <img src={icon} alt="Notificar" />
+      </div>
+    </>
+  )
 }
 
 export default NotificationButton
